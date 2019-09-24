656c4f49da75296996004129453dd75e28b2ab6c
Gabriel Broitman Pinheiro <gabriel.broitman@bndes.gov.br>
2019-09-24 15:43:10 -0300

    Views criadas

--- jsadayapp/views.py
+++ jsadayapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Atividade
 
 # Create your views here.
 def lista_atividade(request):
-    return render(request, 'atividade/lista_atividade.html', {})
+    atividades = Atividade.objects.filter(data_criacao__lte=timezone.now()).order_by('data_criacao')
+    return render(request, 'atividade/lista_atividade.html', {'atividades': atividades})
